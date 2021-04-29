8fef0da03dbf9f1d345d36d0ed8891f6ad70d970
Chris Rohan Yeeda <66123219+RohanYeeda@users.noreply.github.com>
2021-04-29 12:17:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function program()
+function program1()
 {
 var varOne= document.getElementById("value1").value;
 var varTwo= document.getElementById("value2").value;
+callByValue(varOne,varTwo) 
+document.write("After Swapping using CALL BY VALUE &nbsp &nbsp &nbsp &nbsp &nbsp");
 
+document.write("Number 1 :" + varTwo+ "  &nbsp &nbsp "+ "Number 2 :" +varOne);
 
 function callByValue(x, y) { 
-    console.log("PASS BY VALUE")
-    console.log("\n")
+    console.log("PASS BY VALUE");
+    console.log("\n");
     console.log("Before Swapping"); 
     console.log("VALUE1 =" + varOne +" VALUE2 =" +varTwo); 
     let temp;
@@ -20,11 +23,22 @@ function callByValue(x, y) {
   } 
 
 
-callByValue(varOne,varTwo) 
+
+
 
 console.log("\n")
 
-
+}
+function program2()
+{
+  var varOne= document.getElementById("value3").value;
+  var varTwo= document.getElementById("value4").value;
+  let obj = {a:varOne,b:varTwo};
+  callByReference(obj) 
+  document.write("After Swapping USING CALL BY REFERENCE&nbsp &nbsp &nbsp &nbsp &nbsp");
+  document.write("\n")
+  document.write("Number 1 :" + obj.a + " &nbsp &nbsp   "  +"Number 2 :\n" +obj.b)
+  
 
 function callByReference(obj) { 
     console.log("PASS BY REFERENCE")
@@ -39,6 +53,5 @@ function callByReference(obj) {
      console.log(obj);
     
   } 
-let obj = {a:varOne,b:varTwo};
-callByReference(obj) 
+
 }
